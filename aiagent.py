4ba16c997f9021d12cb92adad9a8135e72881b7e
Josh Johnson <caellwyn@gmail.com>
2023-06-09 16:54:54 -0700

    Store variables in AIAgent

--- aiagent.py
+++ aiagent.py
@@ -1,5 +1,6 @@
 import openai
 import streamlit
+
 openai.api_key = streamlit.secrets['OPENAI_API_KEY']
     
 class AIAgent():
@@ -8,16 +9,18 @@ class AIAgent():
         self.system_message = """For each query, consider writings by philosophers that have addressed that question and choose one.
         Respond to the query from the point of view of that philosopher
         Finally, use those referenced sources to form a response to the query that the chosen philosopher would support.
-        Your reponse should be written from the point of view of that philosopher and be sure your response is supported by original sources
-        of text authored by that philosopher.  You should reference those sources in your reponse.
+        Your response should be written from the point of view of that philosopher and be sure your response is supported by original sources
+        of text authored by that philosopher.  You should reference those sources in your response.
         For example:
         Query: What is the meaning of life?
-        Response: Hi, my name is Albert Camus.  In The Myth of Sysyphus, I wrote about how life is meaningless from
+        Response: Hi, my name is Albert Camus.  In The Myth of Sisyphus, I wrote about how life is meaningless from
         an objective viewpoint, but that we can create our own meaning for our lives.  In my works of fiction, I describe
         situations where people must make their own meaning in during times of crisis, doubt and confusion.
         Ultimately, life is what you make of it and it means what it means to you.  One of my most famous quotes is:
         "The meaning of life is whatever you are doing that keeps you from killing yourself."""
+        self.prefix = ''
         self.history = [{'role': 'system', 'content':self.system_message}]
+        self.response = 'Philosophers are waiting patiently, possibly smoking a cigar or pipe'
         
     def add_message(self, text, role):
         message = {'role':role, 'content':text}
@@ -25,37 +28,27 @@ class AIAgent():
         
     def query(self, prompt, temperature=.1):
         # Add user prompt to history
-        self.add_message(prompt, 'user')
+        self.add_message(self.prefix + prompt, 'user')
 
         # Query the model through the API 
-        response = openai.ChatCompletion.create(
+        result = openai.ChatCompletion.create(
             model=self.model,
             messages=self.history,
             temperature=temperature, # this is the degree of randomness of the model's output
         )
-        self.response = response
-        reply = response.choices[0].message["content"]
 
+        self.response = result.choices[0].message["content"]
         # Add reply to message history
-        self.add_message(reply, 'assistant')
+        self.add_message(self.response, 'assistant')
         
-        # Save usage report
-        usage = response['usage']
-        
-
-        # Calculate cost of query
-        if self.model.startswith("gpt-3.5"):
-            cost = (.002 * usage['total_tokens']) / 1000
-        elif self.model == "gpt-4":
-            cost = (usage['prompt_tokens'] * .03 + usage['completion_tokens'] * .06) / 1000
-        else:
-            cost = 'unknown'
-            
+        # set prompt prefix to ensure same philosopher answers each time.
+        self.prefix = 'Please continue to respond as the previous philosopher: '
        
-        return {'content':reply, 'usage':usage, 'cost':cost}
+        # return self.response
     
     def clear_history(self):
         self.history = [{'role':'system', 'content':self.system_message}]
+        self.prefix = ''
         
-    def get_history(self):
+    def get_history():
         return self.history
--- app.py
+++ app.py
@@ -1,61 +1,44 @@
 import streamlit as st
 from aiagent import AIAgent
 
+from streamlit.web.server.websocket_headers import _get_websocket_headers
+import streamlit as st
+
+headers = _get_websocket_headers()
+
+session_id = headers.get("Sec-Websocket-Key")
+
 @st.cache_resource
-def get_agent():
+def get_agent(session_id):
     agent = AIAgent(model='gpt-3.5-turbo')
-    global cost
-    cost = 0
     print('creating the ai agent')
-    st.session_state['prefix'] = ''
-    return agent, cost
+    agent.reponse = 'Philosophers are waiting patiently, possibly smoking a cigar or pipe'
+    return agent
 
 def format_history(history):
     report = ''
-    len_prefix = len(st.session_state['prefix'])
-    print('prefix length', len_prefix)
-    print(f'history length {len(history)}')
     for message in history[1:]:
         if message['role'] == 'user':
-            report += (f"\n\nYOU: {message['content'].replace(st.session_state['prefix'], '')}")
+            report += (f"\n\nYOU: {message['content'].replace(agent.prefix, '')}")
         else:
             report += (f"\n\nPHILOSOPHER: {message['content']}")
     return report
-        
-def clear_history():
-    agent.clear_history()
-    new_philosopher()
-
-def new_philosopher():
-    st.session_state['prefix'] = ''
-
+ 
 def query_agent():
     if prompt:
         try:      
             current_response.write("The forum is considering your query and will send a representative soon.")
-            reply = agent.query(st.session_state['prefix'] + prompt, 
-                                temperature=st.session_state['temperature']
+            agent.query(prompt, 
+                        temperature=temperature
                                 )
-            st.session_state['response'] = reply['content']
-            st.session_state['prefix'] = "Please continue to respond as the previous philosopher: "
+            # print("agent response is: ", response)
         except Exception as e:
-            st.session_state['response'] = "I'm sorry, all philosophers are busy helping other wisdom seekers.  Please try again later."
+            agent.response = "I'm sorry, all philosophers are busy helping other wisdom seekers.  Please try again later."
             print(e)
     else:
         print('no prompt')
 
-agent, cost = get_agent()
-
-if 'prefix' not in st.session_state:
-    st.session_state['prefix'] = ''
-    
 st.title('Chat with a Philosopher!')
-
-st.session_state['temperature'] = st.sidebar.slider('Creativity', min_value=0.0, max_value=1.0, step=0.1, value=0.1)
-st.sidebar.button('New conversation', on_click=clear_history,
-                   use_container_width=False)
-
-st.sidebar.markdown(f'The conversation so far: {format_history(agent.history)}')
 
 prompt = st.text_input(label="Please ask your question and the next available philosopher will answer",
                        max_chars=1000,
@@ -64,12 +47,17 @@ prompt = st.text_input(label="Please ask your question and the next available ph
                        key='user_query',
                        placeholder="Enter your burning question here")
 
-prompt = st.session_state.prefix + prompt
+agent = get_agent(session_id)
 
 st.button('Submit your question to the Forum of Wisdom',
-          on_click=query_agent)
+          on_click=query_agent
+          )
 
-if 'response' not in st.session_state:
-    st.session_state["response"] = 'Philosophers are waiting patiently, possibly smoking a cigar or pipe'
+temperature = st.sidebar.slider('Creativity', min_value=0.0, max_value=1.0, step=0.1, value=0.1)
 
-current_response = st.markdown(st.session_state.response)
+st.sidebar.button('New conversation', on_click=agent.clear_history,
+                   use_container_width=False)
+
+st.sidebar.markdown(f'The conversation so far: {format_history(agent.history)}')
+
+current_response = st.markdown(agent.response)
