4a610dca87031add337c67567b738c158196f3ec
Josh Johnson <caellwyn@gmail.com>
2023-06-09 15:09:22 -0700

    Revert "Update aiagent.py"
    This reverts commit 917f6e02d4d90bac43a36c60e9c3e149cfd7e296.

--- aiagent.py
+++ aiagent.py
@@ -57,12 +57,5 @@ class AIAgent():
     def clear_history(self):
         self.history = [{'role':'system', 'content':self.system_message}]
         
-    @property
-    def history(self):
-        return st.session_state['history']
-    
-    @history.setter
-    def history(self, message):
-        if 'history' in st.session_state:
-            st.session_state['history'].append(message)
-        else: st.session_state['history'] = message
+    def get_history(self):
+        return self.history
