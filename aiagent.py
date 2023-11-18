73bfee02b464ba6aa04cadb6e13b425f7ae2b260
Josh Johnson <caellwyn@gmail.com>
2023-11-18 09:48:03 -0800

    update for new openai release

--- aiagent.py
+++ aiagent.py
@@ -4,9 +4,10 @@ import streamlit
 openai.api_key = streamlit.secrets['OPENAI_API_KEY']
     
 class AIAgent():
-    def __init__(self, model="gpt-3.5-turbo"):
-        """Initialize the AI Agent.  Set the system message, initial prompt prefix, and initial response"""
-        self.model=model
+    def __init__(self, model="gpt-4-1106-preview"):
+        
+        self.agent = openai.OpenAI()
+        self.model = model
         self.system_message = """For each query, consider writings by philosophers that have addressed that question and choose one.
         Respond to the query from the point of view of that philosopher
         Finally, use those referenced sources to form a response to the query that the chosen philosopher would support.
@@ -24,30 +25,32 @@ class AIAgent():
         self.response = 'Philosophers are waiting patiently, possibly smoking a cigar or pipe'
         
     def add_message(self, text, role):
-        """Add a message to the conversation history"""
         message = {'role':role, 'content':text}
         self.history.append(message)
         
     def query(self, prompt, temperature=.1):
         # Add user prompt to history
-        self.add_message(self.prefix + prompt, 'user')
+        self.add_message(prompt + self.prefix, 'user')
 
         # Query the model through the API 
-        result = openai.ChatCompletion.create(
+        self.result = self.agent.chat.completions.create(
             model=self.model,
             messages=self.history,
             temperature=temperature, # this is the degree of randomness of the model's output
         )
 
-        self.response = result.choices[0].message["content"]
+        self.response = self.result.choices[0].message.content
         # Add reply to message history
         self.add_message(self.response, 'assistant')
         
         # set prompt prefix to ensure same philosopher answers each time.
-        self.prefix = 'Please continue to respond as the previous philosopher: '
-           
+        self.prefix = 'Respond as the previous philosopher.  Do not change your persona: '
+       
+        return self.response
+    
     def clear_history(self):
-        """Reset the history to its initial state.  Also reset the prefix and current response"""
         self.history = [{'role':'system', 'content':self.system_message}]
         self.prefix = ''
-        self.response = 'Philosophers are waiting patiently, possibly smoking a cigar or pipe'
+        
+    def get_history():
+        return self.history
